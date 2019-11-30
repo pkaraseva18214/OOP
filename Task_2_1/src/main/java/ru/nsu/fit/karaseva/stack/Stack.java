8c9b78b0af85ec1248ed9e79b3c7c98e3a6567b5
pkaraseva18214 <42939956+pkaraseva18214@users.noreply.github.com>
2019-11-30 16:07:35 +0700

    Updated

--- Task_2_1/src/main/java/ru/nsu/fit/karaseva/stack/Stack.java
+++ Task_2_1/src/main/java/ru/nsu/fit/karaseva/stack/Stack.java
@@ -5,86 +5,93 @@ import java.util.NoSuchElementException;
 
 /**
  * Class that implements the work of the stack.
+ *
  * @param <T> type of the elements.
  */
 public class Stack<T> {
-    private StackElement<T> positionStackElement = null;
-    private StackElement<T> head = null;
-    private int position = 0;
 
-    /**
-     * Method that pushes the element to the stack.
-     * @param element element that we need to put in the stack.
-     */
-    public void push(T element) {
-        position++;
-        if (positionStackElement == null) {
-            positionStackElement = new StackElement<T>(element, null);
-            head = positionStackElement;
-        } else {
-            positionStackElement = new StackElement<>(element, positionStackElement);
+  private StackElement<T> positionStackElement = null;
+  private StackElement<T> head = null;
+  private int position = 0;
+
+  /**
+   * Method that pushes the element to the stack.
+   *
+   * @param element element that we need to put in the stack.
+   */
+  public void push(T element) {
+    position++;
+    if (positionStackElement == null) {
+      positionStackElement = new StackElement<T>(element, null);
+      head = positionStackElement;
+    } else {
+      positionStackElement = new StackElement<>(element, positionStackElement);
+    }
+  }
+
+  /**
+   * Method that gets element from the stack.
+   *
+   * @return element of the stack if it exists, otherwise throws exception.
+   * @throws IndexOutOfBoundsException throws this exception if the stack is empty.
+   */
+  public T pop() throws IndexOutOfBoundsException {
+    if (position == 0) {
+      throw new IndexOutOfBoundsException();
+    } else {
+      T element = positionStackElement.getElement();
+      positionStackElement = positionStackElement.getPreviousElement();
+      if (positionStackElement != null) {
+        positionStackElement.setNextElement(null);
+      }
+      position--;
+      if (position == 0) {
+        head = null;
+      }
+      return element;
+    }
+  }
+
+  /**
+   * Implementation of the Iterator for the stack.
+   *
+   * @return next element of the stack.
+   */
+  public Iterator<T> iterator() {
+    return new Iterator<T>() {
+      private StackElement<T> current = head;
+
+      public boolean hasNext() {
+        return current != null;
+      }
+
+      public T next() throws NoSuchElementException {
+        T result = current.getElement();
+        if (!hasNext()) {
+          throw new NoSuchElementException("End of the stack.");
         }
-    }
+        current = current.getNextElement();
+        return result;
+      }
+    };
+  }
 
-    /**
-     * Method that gets element from the stack.
-     * @return element of the stack if it exists, otherwise throws exception.
-     * @throws IndexOutOfBoundsException throws this exception if the stack is empty.
-     */
-    public T pop() throws IndexOutOfBoundsException {
-        if (position == 0) {
-            throw new IndexOutOfBoundsException();
-        } else {
-            T element = positionStackElement.getElement();
-            positionStackElement = positionStackElement.getPreviousElement();
-            if (positionStackElement != null){
-                positionStackElement.setNextElement(null);
-            }
-            position--;
-            if (position == 0){
-                head = null;
-            }
-            return element;
-        }
-    }
+  /**
+   * Method that gets amount of elements in the stack.
+   *
+   * @return amount of elements in the stack.
+   */
+  public int amountOfElements() {
+    return position;
+  }
 
-    /**
-     *  Implementation of the Iterator for the stack.
-     * @return next element of the stack.
-     */
-    public Iterator<T> iterator() {
-        return new Iterator<T>() {
-            private StackElement<T> current = head;
-
-            public boolean hasNext() {
-                return current != null;
-            }
-
-            public T next() throws NoSuchElementException {
-                T result = current.getElement();
-                if (!hasNext()) {
-                    throw new NoSuchElementException("End of the stack.");
-                }
-                current = current.getNextElement();
-                return result;
-            }
-        };
-    }
-
-    /**
-     * Method that gets amount of elements in the stack.
-     * @return amount of elements in the stack.
-     */
-    public int amountOfElements() {
-        return position;
-    }
-
-    /**
-     * Check if the stack is empty.
-     * @return 1 - if the stack is empty, otherwise returns 0.
-     */
-    public boolean isEmpty() {
-        return position == 0;
-    }
+  /**
+   * Check if the stack is empty.
+   *
+   * @return 1 - if the stack is empty, otherwise returns 0.
+   */
+  public boolean isEmpty() {
+    return position == 0;
+  }
 
 }
